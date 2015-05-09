3da3b2489bf3b69694a50fd5c8fa89afa793f80e
andreaspenz <testa.peta@gmail.com>
2015-05-09 11:38:40 +0200

    remove useless table create/drop column definitions

--- app/code/community/MagentoHackathon/AdvancedAcl/sql/magentohackathon_advancedacl_setup/install-1.0.0.php
+++ app/code/community/MagentoHackathon/AdvancedAcl/sql/magentohackathon_advancedacl_setup/install-1.0.0.php
@@ -5,14 +5,6 @@ $installer = $this;
 
 $installer->startSetup();
 
-$installer->getConnection()->addColumn($installer->getTable('admin/role'), 'magentohackathon_advancedacl', array(
-    'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
-    'length'   => '64k',
-    'nullable' => true,
-    'default'  => null,
-    'comment'  => 'MagentoHackathon Advanced ACL field'
-));
-
 $installer->getConnection()->addColumn($installer->getTable('admin/role'), 'websites', array(
     'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
     'length'   => 255,
@@ -20,7 +12,5 @@ $installer->getConnection()->addColumn($installer->getTable('admin/role'), 'webs
     'default'  => null,
     'comment'  => 'MagentoHackathon Advanced ACL Websites field'
 ));
-
-$installer->getConnection()->dropColumn($installer->getTable('admin/role'), 'magentohackathon_advancedacl');
 
 $installer->endSetup();
