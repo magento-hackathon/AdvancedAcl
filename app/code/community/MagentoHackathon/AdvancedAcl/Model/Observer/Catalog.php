4c77afd45cbcd088256827eab710530ac352f8e9
Michael Lühr <michael.luehr@gmail.com>
2015-05-09 18:10:27 +0200

    filter products

---
+++ app/code/community/MagentoHackathon/AdvancedAcl/Model/Observer/Catalog.php
@@ -0,0 +1,26 @@
+<?php
+
+class MagentoHackathon_AdvancedAcl_Model_Observer_Catalog
+{
+
+    public function filterProductGrid(Varien_Event_Observer $observer)
+    {
+        $collection = $observer->getCollection();
+        if ($collection instanceof Mage_Catalog_Model_Resource_Product_Collection) {
+            $storeIds = $this->getStoreIds();
+            if (!empty($storeIds)) {
+                $collection->addStoreFilter($storeIds);
+            }
+        }
+    }
+
+    /**
+     * retrieves allowed store ids
+     *
+     * @return mixed
+     */
+    protected function getStoreIds()
+    {
+        return Mage::helper('magentohackathon_advancedacl/data')->getActiveRole()->getStoreIds();
+    }
+}
