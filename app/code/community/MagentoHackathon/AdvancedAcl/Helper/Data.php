3d43651ee43c0a9da7f30a220a1e0ca07cef12eb
Andre Flitsch <andre@pixelperfect.at>
2015-05-09 14:30:03 +0200

    Added licenses

--- app/code/community/MagentoHackathon/AdvancedAcl/Helper/Data.php
+++ app/code/community/MagentoHackathon/AdvancedAcl/Helper/Data.php
@@ -1,4 +1,37 @@
 <?php
+/**
+ * MagentoHackathon
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    MagentoHackathon
+ * @package     MagentoHackathon_AdvancedAcl
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+/**
+ * Helper class
+ *
+ * @category    MagentoHackathon
+ * @package     MagentoHackathon_AdvancedAcl
+ * @author      Andreas Penz <testa.peta@gmail.com>
+ * @author      Thomas Birke <tbirke@netextreme.de>
+ */
 
 class MagentoHackathon_AdvancedAcl_Helper_Data
     extends Mage_Core_Helper_Abstract
@@ -14,7 +47,7 @@ class MagentoHackathon_AdvancedAcl_Helper_Data
     protected $_isSingleStoreMode = null;
 
     /**
-     * 
+     *
      * @todo add DispatchEvent in this method
      * @return array
      */
