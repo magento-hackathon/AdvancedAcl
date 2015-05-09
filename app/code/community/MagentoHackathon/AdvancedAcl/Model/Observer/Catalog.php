cf5488e08ac7c4466e7739f39e349128475aa945
Michael Lühr <michael.luehr@gmail.com>
2015-05-09 19:34:47 +0200

    filter products grid

--- app/code/community/MagentoHackathon/AdvancedAcl/Model/Observer/Catalog.php
+++ app/code/community/MagentoHackathon/AdvancedAcl/Model/Observer/Catalog.php
@@ -5,11 +5,16 @@ class MagentoHackathon_AdvancedAcl_Model_Observer_Catalog
 
     public function filterProductGrid(Varien_Event_Observer $observer)
     {
-        $collection = $observer->getCollection();
+        $collection = $observer->getCollection()
+        ->getSelect()->join(array(
+            'product_store' => Mage::getSingleton('core/resource')->getTableName("catalog/product_website")),
+            'e.entity_id = product_store.product_id'
+
+        );
         if ($collection instanceof Mage_Catalog_Model_Resource_Product_Collection) {
             $storeIds = $this->getStoreIds();
             if (!empty($storeIds)) {
-                $collection->addStoreFilter($storeIds);
+                $collection->addFieldToFilter('website_id', array('in' => $storeIds));
             }
         }
     }
