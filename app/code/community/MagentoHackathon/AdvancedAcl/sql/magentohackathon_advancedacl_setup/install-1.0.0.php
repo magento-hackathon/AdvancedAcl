c4179e08a2bd5e19685096e5d244c4ede2603edb
andreaspenz <testa.peta@gmail.com>
2015-05-09 11:48:30 +0200

    create admin role to store linkage table and config

--- app/code/community/MagentoHackathon/AdvancedAcl/sql/magentohackathon_advancedacl_setup/install-1.0.0.php
+++ app/code/community/MagentoHackathon/AdvancedAcl/sql/magentohackathon_advancedacl_setup/install-1.0.0.php
@@ -5,12 +5,26 @@ $installer = $this;
 
 $installer->startSetup();
 
-$installer->getConnection()->addColumn($installer->getTable('admin/role'), 'websites', array(
-    'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
-    'length'   => 255,
-    'nullable' => true,
-    'default'  => null,
-    'comment'  => 'MagentoHackathon Advanced ACL Websites field'
-));
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('magentohackathon_advancedacl/role_store'))
+    ->addColumn('role_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+        'nullable'  => false,
+        'primary'   => true,
+    ), 'Role ID')
+    ->addColumn('store_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ), 'Store ID')
+    ->addIndex($installer->getIdxName('magentohackathon_advancedacl/role_store', array('store_id')),
+        array('store_id'))
+    ->addForeignKey($installer->getFkName('magentohackathon_advancedacl/role_store', 'role_id', 'admin/role', 'role_id'),
+        'role_id', $installer->getTable('admin/role'), 'role_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+    ->addForeignKey($installer->getFkName('magentohackathon_advancedacl/role_store', 'store_id', 'core/store', 'store_id'),
+        'store_id', $installer->getTable('core/store'), 'store_id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+    ->setComment('Admin Role To Store Linkage Table');
+$installer->getConnection()->createTable($table);
 
 $installer->endSetup();
