76e81930e9cbac22ffe6760f0d7fa0963b8eedcd
luemic <michael.luehr@gmail.com>
2015-05-10 10:34:42 +0200

    fix #15 filter out products by allowed store

--- app/code/community/MagentoHackathon/AdvancedAcl/Model/Observer/Catalog.php
+++ app/code/community/MagentoHackathon/AdvancedAcl/Model/Observer/Catalog.php
@@ -3,13 +3,19 @@
 class MagentoHackathon_AdvancedAcl_Model_Observer_Catalog
 {
 
+    /**
+     * filter out products by allowed store
+     *
+     * @param Varien_Event_Observer $observer
+     */
     public function filterProductGrid(Varien_Event_Observer $observer)
     {
         $collection = $observer->getCollection();
         if ($collection instanceof Mage_Catalog_Model_Resource_Product_Collection) {
             $storeIds = $this->getStoreIds();
+
             if (!empty($storeIds)) {
-                $collection->addFieldToFilter('website_id', array('in' => $storeIds));
+                $collection->addStoreFilter(current($storeIds));
             }
         }
     }
