17c9f51d4c3c108d6334677c4bb92aea0d1e545a
luemic <michael.luehr@gmail.com>
2015-05-10 09:59:32 +0200

    fix #5 filter products per website

--- app/code/community/MagentoHackathon/AdvancedAcl/Model/Observer/Catalog.php
+++ app/code/community/MagentoHackathon/AdvancedAcl/Model/Observer/Catalog.php
@@ -5,12 +5,7 @@ class MagentoHackathon_AdvancedAcl_Model_Observer_Catalog
 
     public function filterProductGrid(Varien_Event_Observer $observer)
     {
-        $collection = $observer->getCollection()
-        ->getSelect()->join(array(
-            'product_store' => Mage::getSingleton('core/resource')->getTableName("catalog/product_website")),
-            'e.entity_id = product_store.product_id'
-
-        );
+        $collection = $observer->getCollection();
         if ($collection instanceof Mage_Catalog_Model_Resource_Product_Collection) {
             $storeIds = $this->getStoreIds();
             if (!empty($storeIds)) {
